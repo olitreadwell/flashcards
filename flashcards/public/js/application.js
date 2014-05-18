caa34be9fc41fdd0fbbf1ad776a7da6247be7a33
Oli Treadwell <olitreadwell@gmail.com>
2014-05-18 13:51:46 -0500

    Starting to look into jQuery

--- flashcards/public/js/application.js
+++ flashcards/public/js/application.js
@@ -1,7 +1,28 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+// $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+//     // when the page loads
+//     // console.log you clicked on the header when that occurs
+//     $("#UserGuessInputSubmit").click(function(event) {
+//         event.preventDefault();
+//         $( "#userinputfield" ).css( "background-color", "red" );
+//         // alert("jQuery reaches this line");
+
+//         // var request = $.ajax({
+//         //     url: "/play",
+//         //     type: "POST",
+//         //     data: { id : menuId },
+//         //     dataType: "JSON"
+//         //     complete: //function
+//         //   });
+
+//     });
+
+
+
+
+    // This is called after the document has loaded in its entirety
+    // This guarantees that any elements we bind to will exist on the page
+    // when we try to bind to them
+
+    // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
